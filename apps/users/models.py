ad0655c639a76139ccfa2c3885fa5f144bf3ee76
khasanjon <khasanjon.dev@gmail.com>
2023-11-13 12:34:16 +0500

    add user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,12 @@
-from django.db import models
+from django.db.models import Model, CharField, IntegerField, DateTimeField
 
-# Create your models here.
+
+class User(Model):
+    first_name = CharField(max_length=250, null=True, blank=True)
+    last_name = CharField(max_length=250, null=True, blank=True)
+    username = CharField(max_length=100, null=True, blank=True)
+    telegram_id = IntegerField(unique=True)
+    phone = CharField(max_length=15, unique=True)
+    # date
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True, null=True)
