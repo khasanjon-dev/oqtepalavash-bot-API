edfbcbe94accc78658f4453586703f02cd0ea4db
khasanjon <khasanjon.dev@gmail.com>
2023-11-15 08:48:16 +0500

    update some files

--- root/settings.py
+++ root/settings.py
@@ -89,6 +89,6 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 STATIC_ROOT = os.path.join(BASE_DIR / 'static')
-
+MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
