658076f99efbe9344c49cdec9fdb6669b2387ab3
khasanjon <khasanjon.dev@gmail.com>
2023-11-15 09:46:59 +0500

    update some files

--- apps/users/models/user.py
+++ apps/users/models/user.py
@@ -7,6 +7,7 @@ class User(Model):
     username = CharField(max_length=100, null=True, blank=True)
     telegram_id = IntegerField(unique=True)
     phone = CharField(max_length=15, unique=True)
+    city = CharField(max_length=100)
     # date
     created_at = DateTimeField(auto_now_add=True)
     updated_at = DateTimeField(auto_now=True, null=True)
