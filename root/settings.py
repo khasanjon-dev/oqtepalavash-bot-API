7392ff5949729fb6c364f8d970c64426382f1ac6
khasanjon <khasanjon.dev@gmail.com>
2023-11-15 08:47:07 +0500

    update some files

--- root/settings.py
+++ root/settings.py
@@ -57,7 +57,7 @@ WSGI_APPLICATION = 'root.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'OqtepalavashBot.sqlite3',
     }
 }
 
