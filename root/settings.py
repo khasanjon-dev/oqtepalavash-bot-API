60acdc36746f77cc4b320e912baa571e07209289
khasanjon <khasanjon.dev@gmail.com>
2023-11-15 08:52:52 +0500

    update some files

--- root/settings.py
+++ root/settings.py
@@ -95,3 +95,4 @@ STATIC_ROOT = os.path.join(BASE_DIR / 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR / 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+from .settings_jazzmin import *
