ad13fc026b308fc0e76f0ceb65792f97d1abed24
khasanjon <khasanjon.dev@gmail.com>
2023-12-01 15:13:40 +0500

    update some files

--- apps/users/serializers/user.py
+++ apps/users/serializers/user.py
@@ -1,6 +1,14 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from rest_framework.serializers import Serializer, ModelSerializer
 
 from users.models import User
+
+
+class UserSerializer(Serializer):
+    telegram_id = serializers.IntegerField(required=False)
+    phone = serializers.CharField(required=False)
+    city = serializers.CharField(required=False)
+    language = serializers.CharField(required=False)
 
 
 class UserModelSerializer(ModelSerializer):
