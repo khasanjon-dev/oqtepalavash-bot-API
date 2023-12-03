29253d551327229045038b94ce727c3d1432f2df
khasanjon <khasanjon.dev@gmail.com>
2023-12-03 09:24:56 +0500

    update some files

--- apps/orders/models.py
+++ apps/orders/models.py
@@ -1,3 +1,47 @@
-from django.db import models
+from django.db.models import (CASCADE, CharField, DateTimeField, ForeignKey,
+                              IntegerField, Model, TextChoices, TextField)
 
-# Create your models here.
+from products.models import Product
+from users.models import User
+
+
+class Order(Model):
+    class ReceptionType(TextChoices):
+        DELIVERY = 'delivery', 'Delivery'
+        PICKUP = 'pickup', 'Pickup'
+
+    class PaymentType(TextChoices):
+        CASH = 'cash', 'Cash'
+        CLICK = 'click', 'Click'
+
+    class Status(TextChoices):
+        PENDING = 'pending', 'Pending'
+        COMPLETED = 'completed', 'Completed'
+        CANCEL = 'cancel', 'Cancel'
+        SHIPPED = 'shipped', 'Shipped'
+
+    address = TextField()
+    delivery_price = IntegerField()
+    total_price = IntegerField()
+    created_date = DateTimeField(auto_now_add=True)
+    # choices
+    status = CharField(max_length=20, choices=Status.choices)
+    payment_method = CharField(max_length=5, choices=PaymentType.choices)
+    reception_type = CharField(max_length=8, choices=ReceptionType.choices)
+    # relationships
+    customer = ForeignKey(User, CASCADE, 'orders')
+
+    def __str__(self):
+
+        return self.status + ' ' + self.address
+
+
+class OrderItem(Model):
+    quantity = IntegerField()
+    price = IntegerField()
+    # relationships
+    order = ForeignKey(Order, CASCADE, 'order_items')
+    product = ForeignKey(Product, CASCADE)
+
+    def __str__(self):
+        return self.product.name + ' ' + self.order.address
